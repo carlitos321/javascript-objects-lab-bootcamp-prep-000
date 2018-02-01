b5047dbdbe612502e247e2b96d69e39a530810df
carlitos321 <ca.cabrera321@gmail.com>
2018-02-01 22:53:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function updateObjectWithKeyAndValue(object, key, value) {
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  return playlist.key = value
+  return object.key = value
 }
 
 function deleteFromObjectByKey(object, key, value) {
