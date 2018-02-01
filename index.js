9dbed141df82177c5ecd34473d456d5caabcaa42
carlitos321 <ca.cabrera321@gmail.com>
2018-02-01 23:00:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,12 +7,15 @@ const recipes = {
 
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  return object.assign({}, object, {[key]: value} )
+  return Object.assign({}, object, {[key]: value} )
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  object.key = value
-  return object
+  function() {
+    Object.key = value
+    return Object
+  }
+
 }
 
 function deleteFromObjectByKey(object, key, value) {
